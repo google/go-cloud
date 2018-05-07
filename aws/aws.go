2dff51f6a3f58e956b72f451488302d9c8ca8417
Ross Light <light@google.com>
2018-05-07 19:01:06 -0400

    aws: create default provider sets for AWS
    This includes the awscloud.AWS "default" provider set, like
    gcpcloud.GCP, as well as the aws package with provider sets for getting
    the default session.
    
    Change-Id: I0c0cb48c22c18e1c253d36614e66be2bf8f00050
    Reviewed-on: https://team-review.git.corp.google.com/258841
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Tuo Shan <shantuo@google.com>

---
+++ aws/aws.go
@@ -0,0 +1,50 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package aws provides fundamental Goose providers for Amazon Web Services (AWS).
+package aws
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/client"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/google/go-cloud/goose"
+)
+
+// Set is a Goose provider set that provides a variety of common types
+// from the AWS session.
+var Set = goose.NewSet(
+	SessionConfig,
+	ConfigCredentials,
+	goose.Bind(client.ConfigProvider(nil), (*session.Session)(nil)),
+)
+
+// DefaultSession is a Goose provider set that provides a *session.Session using
+// the default options. It includes all the providers in aws.Set.
+var DefaultSession = goose.NewSet(
+	Set,
+	session.NewSessionWithOptions,
+	goose.Value(session.Options{}),
+)
+
+// SessionConfig returns sess.Config.
+func SessionConfig(sess *session.Session) *aws.Config {
+	return sess.Config
+}
+
+// ConfigCredentials returns cfg.Credentials.
+func ConfigCredentials(cfg *aws.Config) *credentials.Credentials {
+	return cfg.Credentials
+}
---
+++ aws/awscloud/awscloud.go
@@ -0,0 +1,45 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package awscloud contains Goose providers for wiring up AWS.
+package awscloud
+
+import (
+	"net/http"
+
+	"github.com/google/go-cloud/aws"
+	"github.com/google/go-cloud/gcp"
+	"github.com/google/go-cloud/goose"
+	"github.com/google/go-cloud/mysql/rdsmysql"
+	"github.com/google/go-cloud/runtimevar/paramstore"
+	"github.com/google/go-cloud/server/sdserver"
+)
+
+// AWS is a Goose provider set that includes all Amazon Web Services interface
+// implementations in go-cloud and authenticates using the default session.
+var AWS = goose.NewSet(
+	Services,
+	aws.DefaultSession,
+	gcp.DefaultIdentity, // needed for Stackdriver
+	goose.Value(http.DefaultClient),
+)
+
+// Services is a Goose provider set that includes the default wiring for all
+// Amazon Web Services interface implementations in go-cloud, but unlike the AWS
+// set, does not include credentials. Individual services may require additional
+// configuration.
+var Services = goose.NewSet(
+	rdsmysql.Set,
+	paramstore.NewClient,
+	sdserver.Set)
