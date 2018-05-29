03f2a403f3e4a64e4e079f1bdc9d542d953b7e54
Ross Light <light@google.com>
2018-05-29 13:36:11 -0700

    all: add hello world snippets for AWS and GCP (#42)
    * all: add hello world snippets for AWS and GCP

---
+++ gcp/gcpcloud/example_test.go
@@ -0,0 +1,75 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gcpcloud_test
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/google/go-cloud/gcp/gcpcloud"
+	"github.com/google/go-cloud/goose"
+	"github.com/google/go-cloud/health"
+	"github.com/google/go-cloud/server"
+	"go.opencensus.io/trace"
+)
+
+// This is an example of how to bootstrap an HTTP server running on
+// Google Cloud Platform (GCP). The code in this function would be
+// placed in main().
+func Example() {
+	// Connect and authenticate to GCP.
+	srv, cleanup, err := setup(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cleanup()
+
+	// Set up the HTTP routes.
+	http.HandleFunc("/", greet)
+
+	// Run the server. This behaves much like http.ListenAndServe,
+	// including that passing a nil handler will use http.DefaultServeMux.
+	log.Fatal(srv.ListenAndServe(":8080", nil))
+}
+
+// setup is a Goose injector function that creates an HTTP server
+// configured to send diagnostics to Stackdriver. The second return
+// value is a clean-up function that can be called to shut down any
+// resources created by setup.
+//
+// The body of this function will be filled in by running Goose. While
+// the name of the function does not matter, the signature signals to
+// Goose what provider functions to call. See
+// https://github.com/google/go-cloud/blob/master/goose/README.md#injectors
+// for more details.
+func setup(ctx context.Context) (*server.Server, func(), error) {
+	panic(goose.Build(
+		// The GCP set includes all the default wiring for GCP, including
+		// for *server.Server.
+		gcpcloud.GCP,
+		// Providing nil instructs the server to use the default sampling policy.
+		goose.Value(trace.Sampler(nil)),
+		// Health checks can be added to delay your server reporting healthy
+		// to the load balancer before critical dependencies are available.
+		goose.Value([]health.Checker{}),
+	))
+}
+
+// greet is an ordinary http.HandleFunc.
+func greet(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
