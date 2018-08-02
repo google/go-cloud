c3014a15f6d5288fd5ef1e8f8cdbc437b48643af
Issac Trotts <issactrotts@google.com>
2018-08-02 16:31:23 -0700

    Revert "Update wire_test to use .golden/"
    This reverts commit 333fe9038299ab7ad56882077aa70849f1d3db08.

--- wire/internal/wire/wire_test.go
+++ wire/internal/wire/wire_test.go
@@ -61,17 +61,14 @@ func TestWire(t *testing.T) {
 	}
 	wd := filepath.Join(magicGOPATH(), "src")
 
-	// The golden directory has a dot prefix so its contents won't be found
-	// by go test.
-	goldenDir := ".golden"
-	if err := os.MkdirAll(goldenDir, 0777); err != nil {
+	if err := os.MkdirAll("golden", 0777); err != nil {
 		t.Fatalf("Failed to create golden dir for checked Wire output: %v", err)
 	}
 
 	// goldenPath constructs the path to the golden output for the test
 	// with the given name.
 	goldenPath := func(name string) string {
-		return filepath.Join(goldenDir, name) + ".go"
+		return filepath.Join("golden", name) + ".go"
 	}
 
 	if *setup.Record {
