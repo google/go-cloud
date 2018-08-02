4346000b974833fd8c9eeedea5ed0ae3da1fe80b
Issac Trotts <issactrotts@google.com>
2018-08-02 11:55:16 -0700

    Use mysql.NewConfig for correct defaults.
    Fixes #275
    
    See a similar change at https://github.com/GoogleCloudPlatform/cloudsql-proxy/pull/172.

---
+++ mysql/cloudmysql/cloudmysql_test.go
@@ -0,0 +1,12 @@
+package cloudmysql
+
+import "testing"
+
+// TestMakeConfig checks for regression of https://github.com/google/go-cloud/issues/275.
+func TestMakeConfig(t *testing.T) {
+	p := Params{}
+	cfg := makeConfig("", &p)
+	if !cfg.AllowNativePasswords {
+		t.Errorf("cfg.AllowNativePasswords is false")
+	}
+}
