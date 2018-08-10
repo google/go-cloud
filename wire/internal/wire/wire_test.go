4118696883a2ce5ce04231b427d454ac122c38b5
Tuo Shan <shantuo@google.com>
2018-08-10 11:31:19 -0700

    run wire tests in parallel

--- wire/internal/wire/wire_test.go
+++ wire/internal/wire/wire_test.go
@@ -70,6 +70,8 @@ func TestWire(t *testing.T) {
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
 			// Run Wire from a fake build context.
 			bctx := test.buildContext()
 			gen, errs := Generate(bctx, wd, test.pkg)
