2440b4140f4eda412b9a6dd10eb5df7c013755ff
Robert van Gent <rvangent@google.com>
2018-08-13 12:26:58 -0700

    Call t.Run for TestRead, TestWrite, etc. from RunConformanceTests instead of from inside each test function.

--- blob/drivertest/drivertest.go
+++ blob/drivertest/drivertest.go
@@ -51,15 +51,18 @@ type BucketMaker func(t *testing.T) (*blob.Bucket, func())
 // RunConformanceTests runs conformance tests for provider implementations
 // of blob.
 func RunConformanceTests(t *testing.T, makeBkt BucketMaker) {
-	tests := []func(*testing.T, BucketMaker){
-		testRead,
-		testWrite,
-		testAttributes,
-		testDelete,
-	}
-	for _, testFn := range tests {
-		testFn(t, makeBkt)
-	}
+	t.Run("TestRead", func(t *testing.T) {
+		testRead(t, makeBkt)
+	})
+	t.Run("TestWrite", func(t *testing.T) {
+		testWrite(t, makeBkt)
+	})
+	t.Run("TestAttributes", func(t *testing.T) {
+		testAttributes(t, makeBkt)
+	})
+	t.Run("TestDelete", func(t *testing.T) {
+		testDelete(t, makeBkt)
+	})
 }
 
 // testRead tests the functionality of NewReader, NewRangeReader, and Reader.
@@ -122,63 +125,61 @@ func testRead(t *testing.T, makeBkt BucketMaker) {
 	}
 
 	ctx := context.Background()
-	t.Run("TestRead", func(t *testing.T) {
 
-		// Create a blob to be read by sub-tests below.
-		b, done := makeBkt(t)
-		defer done()
+	// Create a blob to be read by sub-tests below.
+	b, done := makeBkt(t)
+	defer done()
 
-		w, err := b.NewWriter(ctx, key, nil)
-		if err != nil {
-			t.Fatal(err)
-		}
-		_, err = w.Write(content)
-		if err == nil {
-			err = w.Close()
-		}
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer func() {
-			_ = b.Delete(ctx, key)
-		}()
+	w, err := b.NewWriter(ctx, key, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = w.Write(content)
+	if err == nil {
+		err = w.Close()
+	}
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		_ = b.Delete(ctx, key)
+	}()
 
-		for _, tc := range tests {
-			t.Run(tc.name, func(t *testing.T) {
-				b, done := makeBkt(t)
-				defer done()
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			b, done := makeBkt(t)
+			defer done()
 
-				r, err := b.NewRangeReader(ctx, tc.key, tc.offset, tc.length)
-				if (err != nil) != tc.wantErr {
-					t.Errorf("got err %v want error %v", err, tc.wantErr)
+			r, err := b.NewRangeReader(ctx, tc.key, tc.offset, tc.length)
+			if (err != nil) != tc.wantErr {
+				t.Errorf("got err %v want error %v", err, tc.wantErr)
+			}
+			if err != nil {
+				return
+			}
+			defer r.Close()
+			// Make the buffer bigger than needed to make sure we actually only read
+			// the expected number of bytes.
+			got := make([]byte, tc.wantReadSize+10)
+			n, err := r.Read(got)
+			if err != nil {
+				t.Errorf("unexpected error during read: %v", err)
+			}
+			if int64(n) != tc.wantReadSize {
+				t.Errorf("got read length %d want %d", n, tc.wantReadSize)
+			}
+			if !cmp.Equal(got[:tc.wantReadSize], tc.want) {
+				t.Errorf("got %q want %q", string(got), string(tc.want))
+			}
+			// TODO(issue #305): Fails for S3.
+			/*
+				if r.Size() != contentSize {
+					t.Errorf("got size %d want %d", r.Size(), contentSize)
 				}
-				if err != nil {
-					return
-				}
-				defer r.Close()
-				// Make the buffer bigger than needed to make sure we actually only read
-				// the expected number of bytes.
-				got := make([]byte, tc.wantReadSize+10)
-				n, err := r.Read(got)
-				if err != nil {
-					t.Errorf("unexpected error during read: %v", err)
-				}
-				if int64(n) != tc.wantReadSize {
-					t.Errorf("got read length %d want %d", n, tc.wantReadSize)
-				}
-				if !cmp.Equal(got[:tc.wantReadSize], tc.want) {
-					t.Errorf("got %q want %q", string(got), string(tc.want))
-				}
-				// TODO(issue #305): Fails for S3.
-				/*
-					if r.Size() != contentSize {
-						t.Errorf("got size %d want %d", r.Size(), contentSize)
-					}
-				*/
-				r.Close()
-			})
-		}
-	})
+			*/
+			r.Close()
+		})
+	}
 }
 
 // testAttributes tests the behavior of attributes returned by Reader.
@@ -190,85 +191,83 @@ func testAttributes(t *testing.T, makeBkt BucketMaker) {
 	content := []byte("Hello World!")
 
 	ctx := context.Background()
-	t.Run("Attributes", func(t *testing.T) {
 
-		// Create a blob to be read by sub-tests below.
-		b, done := makeBkt(t)
-		defer done()
+	// Create a blob to be read by sub-tests below.
+	b, done := makeBkt(t)
+	defer done()
 
-		opts := &blob.WriterOptions{
-			ContentType: contentType,
-		}
-		w, err := b.NewWriter(ctx, key, opts)
+	opts := &blob.WriterOptions{
+		ContentType: contentType,
+	}
+	w, err := b.NewWriter(ctx, key, opts)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = w.Write(content)
+	if err == nil {
+		err = w.Close()
+	}
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		_ = b.Delete(ctx, key)
+	}()
+
+	t.Run("ContentType", func(t *testing.T) {
+		r, err := b.NewRangeReader(ctx, key, 0, 0)
 		if err != nil {
-			t.Fatal(err)
+			t.Fatalf("failed NewRangeReader: %v", err)
 		}
-		_, err = w.Write(content)
-		if err == nil {
-			err = w.Close()
+		defer r.Close()
+		if r.ContentType() != contentType {
+			t.Errorf("got ContentType %q want %q", r.ContentType(), contentType)
 		}
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer func() {
-			_ = b.Delete(ctx, key)
-		}()
+	})
 
-		t.Run("ContentType", func(t *testing.T) {
+	// TODO(issue #303): Fails for GCS.
+	/*
+		t.Run("Size", func(t *testing.T) {
 			r, err := b.NewRangeReader(ctx, key, 0, 0)
 			if err != nil {
-				t.Fatalf("failed NewRangeReader: %v", err)
+				t.Errorf("failed NewRangeReader: %v", err)
 			}
 			defer r.Close()
-			if r.ContentType() != contentType {
-				t.Errorf("got ContentType %q want %q", r.ContentType(), contentType)
+			if r.Size() != int64(len(content)) {
+				t.Errorf("got Size %d want %d", r.Size(), len(content))
 			}
 		})
+	*/
 
-		// TODO(issue #303): Fails for GCS.
-		/*
-			t.Run("Size", func(t *testing.T) {
-				r, err := b.NewRangeReader(ctx, key, 0, 0)
-				if err != nil {
-					t.Errorf("failed NewRangeReader: %v", err)
-				}
-				defer r.Close()
-				if r.Size() != int64(len(content)) {
-					t.Errorf("got Size %d want %d", r.Size(), len(content))
-				}
-			})
-		*/
-
-		t.Run("ModTime", func(t *testing.T) {
-			r, err := b.NewRangeReader(ctx, key, 0, 0)
-			if err != nil {
-				t.Fatalf("failed NewRangeReader: %v", err)
-			}
-			defer r.Close()
-			t1 := r.ModTime()
-			if t1.IsZero() {
-				// This provider doesn't support ModTime.
-				return
-			}
-			// Touch the file after a couple of seconds and make sure ModTime changes.
-			time.Sleep(2 * time.Second)
-			w, err := b.NewWriter(ctx, key, nil)
-			if err != nil {
-				t.Fatalf("failed NewWriter: %v", err)
-			}
-			if err = w.Close(); err != nil {
-				t.Errorf("failed NewWriter Close: %v", err)
-			}
-			r2, err := b.NewRangeReader(ctx, key, 0, 0)
-			if err != nil {
-				t.Errorf("failed NewRangeReader#2: %v", err)
-			}
-			defer r2.Close()
-			t2 := r2.ModTime()
-			if !t2.After(t1) {
-				t.Errorf("ModTime %v is not after %v", t2, t1)
-			}
-		})
+	t.Run("ModTime", func(t *testing.T) {
+		r, err := b.NewRangeReader(ctx, key, 0, 0)
+		if err != nil {
+			t.Fatalf("failed NewRangeReader: %v", err)
+		}
+		defer r.Close()
+		t1 := r.ModTime()
+		if t1.IsZero() {
+			// This provider doesn't support ModTime.
+			return
+		}
+		// Touch the file after a couple of seconds and make sure ModTime changes.
+		time.Sleep(2 * time.Second)
+		w, err := b.NewWriter(ctx, key, nil)
+		if err != nil {
+			t.Fatalf("failed NewWriter: %v", err)
+		}
+		if err = w.Close(); err != nil {
+			t.Errorf("failed NewWriter Close: %v", err)
+		}
+		r2, err := b.NewRangeReader(ctx, key, 0, 0)
+		if err != nil {
+			t.Errorf("failed NewRangeReader#2: %v", err)
+		}
+		defer r2.Close()
+		t2 := r2.ModTime()
+		if !t2.After(t1) {
+			t.Errorf("ModTime %v is not after %v", t2, t1)
+		}
 	})
 }
 
@@ -356,64 +355,61 @@ func testWrite(t *testing.T, makeBkt BucketMaker) {
 	}
 
 	ctx := context.Background()
-	t.Run("TestWrite", func(t *testing.T) {
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			b, done := makeBkt(t)
+			defer done()
 
-		for _, tc := range tests {
-			t.Run(tc.name, func(t *testing.T) {
-				b, done := makeBkt(t)
-				defer done()
-
-				// Write the content.
-				opts := &blob.WriterOptions{
-					ContentType: tc.contentType,
-				}
-				w, err := b.NewWriter(ctx, tc.key, opts)
-				if err == nil {
-					if len(tc.content) > 0 {
-						if tc.firstChunk == 0 {
-							// Write the whole thing.
-							_, err = w.Write(tc.content)
-						} else {
-							// Write it in 2 chunks.
-							_, err = w.Write(tc.content[:tc.firstChunk])
-							if err == nil {
-								_, err = w.Write(tc.content[tc.firstChunk:])
-							}
+			// Write the content.
+			opts := &blob.WriterOptions{
+				ContentType: tc.contentType,
+			}
+			w, err := b.NewWriter(ctx, tc.key, opts)
+			if err == nil {
+				if len(tc.content) > 0 {
+					if tc.firstChunk == 0 {
+						// Write the whole thing.
+						_, err = w.Write(tc.content)
+					} else {
+						// Write it in 2 chunks.
+						_, err = w.Write(tc.content[:tc.firstChunk])
+						if err == nil {
+							_, err = w.Write(tc.content[tc.firstChunk:])
 						}
 					}
-					if err == nil {
-						err = w.Close()
-					}
 				}
-				if (err != nil) != tc.wantErr {
-					t.Errorf("NewWriter or Close got err %v want error %v", err, tc.wantErr)
+				if err == nil {
+					err = w.Close()
 				}
-				if err != nil {
-					return
-				}
-				defer func() { _ = b.Delete(ctx, tc.key) }()
+			}
+			if (err != nil) != tc.wantErr {
+				t.Errorf("NewWriter or Close got err %v want error %v", err, tc.wantErr)
+			}
+			if err != nil {
+				return
+			}
+			defer func() { _ = b.Delete(ctx, tc.key) }()
 
-				// Read it back.
-				r, err := b.NewReader(ctx, tc.key)
-				if err != nil {
-					t.Fatalf("failed to NewReader: %v", err)
+			// Read it back.
+			r, err := b.NewReader(ctx, tc.key)
+			if err != nil {
+				t.Fatalf("failed to NewReader: %v", err)
+			}
+			defer r.Close()
+			buf := make([]byte, len(tc.content))
+			_, err = r.Read(buf)
+			if err != nil {
+				t.Errorf("failed to Read: %v", err)
+			}
+			if !bytes.Equal(buf, tc.content) {
+				if len(buf) < 100 && len(tc.content) < 100 {
+					t.Errorf("read didn't match write; got \n%s\n want \n%s", string(buf), string(tc.content))
+				} else {
+					t.Error("read didn't match write, content too large to display")
 				}
-				defer r.Close()
-				buf := make([]byte, len(tc.content))
-				_, err = r.Read(buf)
-				if err != nil {
-					t.Errorf("failed to Read: %v", err)
-				}
-				if !bytes.Equal(buf, tc.content) {
-					if len(buf) < 100 && len(tc.content) < 100 {
-						t.Errorf("read didn't match write; got \n%s\n want \n%s", string(buf), string(tc.content))
-					} else {
-						t.Error("read didn't match write, content too large to display")
-					}
-				}
-			})
-		}
-	})
+			}
+		})
+	}
 }
 
 // testDelete tests the functionality of Delete.
@@ -421,52 +417,49 @@ func testDelete(t *testing.T, makeBkt BucketMaker) {
 	const key = "blob-for-deleting"
 
 	ctx := context.Background()
-	t.Run("Delete", func(t *testing.T) {
+	t.Run("NonExistentFails", func(t *testing.T) {
+		b, done := makeBkt(t)
+		defer done()
+		err := b.Delete(ctx, "does-not-exist")
+		if err == nil {
+			t.Errorf("want error, got nil")
+		} else if !blob.IsNotExist(err) {
+			t.Errorf("want IsNotExist error, got %v", err)
+		}
+	})
 
-		t.Run("NonExistentFails", func(t *testing.T) {
-			b, done := makeBkt(t)
-			defer done()
-			err := b.Delete(ctx, "does-not-exist")
-			if err == nil {
-				t.Errorf("want error, got nil")
-			} else if !blob.IsNotExist(err) {
-				t.Errorf("want IsNotExist error, got %v", err)
-			}
-		})
-
-		t.Run("Works", func(t *testing.T) {
-			b, done := makeBkt(t)
-			defer done()
-			// Create the blob.
-			writer, err := b.NewWriter(ctx, key, nil)
-			if err != nil {
-				t.Fatalf("failed to NewWriter: %v", err)
-			}
-			_, err = io.WriteString(writer, "Hello world")
-			if err != nil {
-				t.Errorf("failed to write: %v", err)
-			}
-			if err := writer.Close(); err != nil {
-				t.Errorf("failed to close Writer: %v", err)
-			}
-			// Delete it.
-			if err := b.Delete(ctx, key); err != nil {
-				t.Errorf("got unexpected error deleting blob: %v", err)
-			}
-			// Subsequent read fails with IsNotExist.
-			_, err = b.NewReader(ctx, key)
-			if err == nil {
-				t.Errorf("read after delete want error, got nil")
-			} else if !blob.IsNotExist(err) {
-				t.Errorf("read after delete want IsNotExist error, got %v", err)
-			}
-			// Subsequent delete also fails.
-			err = b.Delete(ctx, key)
-			if err == nil {
-				t.Errorf("delete after delete want error, got nil")
-			} else if !blob.IsNotExist(err) {
-				t.Errorf("delete after delete want IsNotExist error, got %v", err)
-			}
-		})
+	t.Run("Works", func(t *testing.T) {
+		b, done := makeBkt(t)
+		defer done()
+		// Create the blob.
+		writer, err := b.NewWriter(ctx, key, nil)
+		if err != nil {
+			t.Fatalf("failed to NewWriter: %v", err)
+		}
+		_, err = io.WriteString(writer, "Hello world")
+		if err != nil {
+			t.Errorf("failed to write: %v", err)
+		}
+		if err := writer.Close(); err != nil {
+			t.Errorf("failed to close Writer: %v", err)
+		}
+		// Delete it.
+		if err := b.Delete(ctx, key); err != nil {
+			t.Errorf("got unexpected error deleting blob: %v", err)
+		}
+		// Subsequent read fails with IsNotExist.
+		_, err = b.NewReader(ctx, key)
+		if err == nil {
+			t.Errorf("read after delete want error, got nil")
+		} else if !blob.IsNotExist(err) {
+			t.Errorf("read after delete want IsNotExist error, got %v", err)
+		}
+		// Subsequent delete also fails.
+		err = b.Delete(ctx, key)
+		if err == nil {
+			t.Errorf("delete after delete want error, got nil")
+		} else if !blob.IsNotExist(err) {
+			t.Errorf("delete after delete want IsNotExist error, got %v", err)
+		}
 	})
 }
