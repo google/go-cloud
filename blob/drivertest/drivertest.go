6ba7b361c9f3b52fb6ec9d55bdecc2984f50edcd
Robert van Gent <rvangent@google.com>
2018-08-14 11:51:05 -0700

    Expand documentation on required semantics for makeBucket.

--- blob/drivertest/drivertest.go
+++ blob/drivertest/drivertest.go
@@ -25,6 +25,18 @@
 // func TestConformance(t *testing.T) {
 //   drivertest.RunConformanceTests(t, makeBucket)
 // }
+//
+// Multiple calls to makeBucket during a RunConformanceTests run must refer to
+// the same storage bucket; i.e., a blob created using one *blob.Bucket must be
+// readable by a subsequent *blob.Bucket. This is required so that tests can,
+// for example, create a blob once and then run a series of tests over it; see
+// testRead below for an example.
+//
+// Implementation note: A new *blob.BucketEach is created for each test and
+// sub-test so that provider implementations that use a record/replay strategy
+// get a separate golden file per test. For example, testRead will create a
+// replay file for the blob creation/deletion, and separate replay files for
+// each sub-test.
 package drivertest
 
 import (
@@ -41,11 +53,10 @@ import (
 )
 
 // BucketMaker describes functions used to create a bucket for a test.
-// Multiple calls to BucketMaker during a test run must refer to the same
-// storage bucket.
 // Functions should return the bucket along with a "done" function to be called
 // when the test is complete.
 // If bucket creation fails, functions should t.Fatal to stop the test.
+// See the package comments for more details on required semantics.
 type BucketMaker func(t *testing.T) (*blob.Bucket, func())
 
 // RunConformanceTests runs conformance tests for provider implementations
