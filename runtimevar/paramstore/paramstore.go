09a8805daa96a831235e140edd005fb1804f77ff
Robert van Gent <rvangent@google.com>
2018-08-24 10:42:22 -0700

    runtimevar: Move dedupe detection and poll-wait into runtimevar.
    This simplifies the provider implementations significantly.

--- runtimevar/paramstore/paramstore.go
+++ runtimevar/paramstore/paramstore.go
@@ -22,7 +22,6 @@ import (
 
 	"github.com/google/go-cloud/runtimevar"
 	"github.com/google/go-cloud/runtimevar/driver"
-	"github.com/google/go-cloud/runtimevar/internal"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
@@ -61,11 +60,10 @@ func (c *Client) NewVariable(name string, decoder *runtimevar.Decoder, opts *Wat
 	}
 
 	return runtimevar.New(&watcher{
-		sess:        c.sess,
-		name:        name,
-		waitTime:    waitTime,
-		decoder:     decoder,
-		lastVersion: -1,
+		sess:     c.sess,
+		name:     name,
+		waitTime: waitTime,
+		decoder:  decoder,
 	}), nil
 }
 
@@ -86,52 +84,54 @@ type watcher struct {
 	// waitTime is the amount of time to wait between querying AWS.
 	waitTime time.Duration
 	// decoder is the decoder that unmarshals the value in the param.
-	decoder     *runtimevar.Decoder
-	lastVersion int64
-	lastErrCode string
+	decoder *runtimevar.Decoder
 }
 
-// WatchVariable begins watching the given parameter and waiting for it to change.
-// The function will block until either the parameter changes, or the context
-// is canceled.
-func (w *watcher) WatchVariable(ctx context.Context) (driver.Variable, error) {
-	return internal.Pinger(ctx, w.ping, w.waitTime)
+func (w *watcher) WatchVariable(ctx context.Context, prevVersion interface{}, prevErr error) (*driver.Variable, interface{}, time.Duration, error) {
+
+	// checkSameErr checks to see if err is the same as prevErr, andif so, returns
+	// the "no change" signal with w.waitTime.
+	checkSameErr := func(err error) (*driver.Variable, interface{}, time.Duration, error) {
+		if prevErr != nil {
+			var code, prevCode string
+			if awsErr, ok := err.(awserr.Error); ok {
+				code = awsErr.Code()
+			}
+			if awsErr, ok := prevErr.(awserr.Error); ok {
+				prevCode = awsErr.Code()
+			}
+			if (code != "" && code == prevCode) || err.Error() == prevErr.Error() {
+				return nil, nil, w.waitTime, nil
+			}
+		}
+		return nil, nil, 0, err
+	}
+
+	lastVersion := int64(-1)
+	if prevVersion != nil {
+		lastVersion = prevVersion.(int64)
+	}
+	// Read the variable from the backend.
+	p, err := readParam(w.sess, w.name, lastVersion)
+	if err != nil {
+		return checkSameErr(err)
+	}
+	if p == nil {
+		// Version hasn't changed.
+		return nil, nil, w.waitTime, nil
+	}
+
+	// New value! Decode it.
+	val, err := w.decoder.Decode([]byte(p.value))
+	if err != nil {
+		return checkSameErr(err)
+	}
+	return &driver.Variable{Value: val, UpdateTime: p.updateTime}, p.version, 0, nil
 }
 
 // Close is a no-op. Cancel the context passed to Watch if watching should end.
 func (w *watcher) Close() error {
 	return nil
-}
-
-// ping hits AWS to read the latest parameter. If nothing had changed,
-// the Variable and error will be nil.
-func (w *watcher) ping(_ context.Context) (*driver.Variable, error) {
-	p, err := readParam(w.sess, w.name, w.lastVersion)
-	if err != nil {
-		errCode := err.Error()
-		if awsErr, ok := err.(awserr.Error); ok {
-			errCode = awsErr.Code()
-		}
-		if w.lastErrCode != "" && errCode == w.lastErrCode {
-			// No change.
-			return nil, nil
-		}
-		w.lastErrCode = errCode
-		w.lastVersion = -1
-		return nil, err
-	}
-	w.lastErrCode = ""
-	// version is set to 0 by readParam if the version hasn't changed.
-	if p.version != 0 && w.lastVersion != p.version {
-		dec := w.decoder
-		val, err := dec.Decode([]byte(p.value))
-		if err != nil {
-			return nil, err
-		}
-		w.lastVersion = p.version
-		return &driver.Variable{Value: val, UpdateTime: p.updateTime}, nil
-	}
-	return nil, nil
 }
 
 type param struct {
@@ -143,22 +143,21 @@ type param struct {
 
 // readParam returns the named parameter. An error is returned if AWS is unreachable
 // or the named parameter isn't found. If the parameter hasn't changed from the
-// passed version, returns an empty param {} and nil error. This saves a
+// passed version, returns a nil param and nil error. This saves a
 // redundant API call.
-func readParam(p client.ConfigProvider, name string, version int64) (param, error) {
-	zeroParam := param{}
+func readParam(p client.ConfigProvider, name string, version int64) (*param, error) {
 	svc := ssm.New(p)
 
 	getResp, err := svc.GetParameter(&ssm.GetParameterInput{Name: aws.String(name)})
 	if err != nil {
-		return zeroParam, err
+		return nil, err
 	}
 	if getResp.Parameter == nil {
-		return zeroParam, fmt.Errorf("unable to get %q parameter", name)
+		return nil, fmt.Errorf("unable to get %q parameter", name)
 	}
 	getP := getResp.Parameter
 	if *getP.Version == version {
-		return zeroParam, nil
+		return nil, nil
 	}
 
 	descResp, err := svc.DescribeParameters(&ssm.DescribeParametersInput{
@@ -167,14 +166,14 @@ func readParam(p client.ConfigProvider, name string, version int64) (param, erro
 		},
 	})
 	if err != nil {
-		return zeroParam, err
+		return nil, err
 	}
 	if len(descResp.Parameters) != 1 || *descResp.Parameters[0].Name != name {
-		return zeroParam, fmt.Errorf("unable to get single %q parameter", name)
+		return nil, fmt.Errorf("unable to get single %q parameter", name)
 	}
 	descP := descResp.Parameters[0]
 
-	return param{
+	return &param{
 		name:       *getP.Name,
 		value:      *getP.Value,
 		version:    *getP.Version,
