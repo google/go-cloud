95be1fd31dcdbc5c3cc7348978956261cae714c5
Garrett Squire <garrettsquire@gmail.com>
2018-10-09 21:22:02 -0700

    consolidate the header writing into a shared function

--- health/health.go
+++ health/health.go
@@ -18,6 +18,7 @@ package health
 import (
 	"io"
 	"net/http"
+	"strconv"
 )
 
 // Handler is an HTTP handler that reports on the success of an
@@ -42,12 +43,18 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, _ *http.Request) {
 	writeHealthy(w)
 }
 
-func writeUnhealthy(w http.ResponseWriter) {
-	w.Header().Set("Content-Length", "9")
+func writeHeaders(status string, w http.ResponseWriter) {
+	w.Header().Set("Content-Length", strconv.Itoa(len(status)))
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.Header().Set("X-Content-Type-Options", "nosniff")
+}
+
+func writeUnhealthy(w http.ResponseWriter) {
+	const status = "unhealthy"
+
+	writeHeaders(status, w)
 	w.WriteHeader(http.StatusInternalServerError)
-	io.WriteString(w, "unhealthy")
+	io.WriteString(w, status)
 }
 
 // HandleLive is an http.HandleFunc that handles liveness checks by
@@ -57,11 +64,11 @@ func HandleLive(w http.ResponseWriter, _ *http.Request) {
 }
 
 func writeHealthy(w http.ResponseWriter) {
-	w.Header().Set("Content-Length", "2")
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.Header().Set("X-Content-Type-Options", "nosniff")
+	const status = "ok"
+
+	writeHeaders(status, w)
 	w.WriteHeader(http.StatusOK)
-	io.WriteString(w, "ok")
+	io.WriteString(w, status)
 }
 
 // Checker wraps the CheckHealth method.
