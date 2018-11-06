1e5d8d6771f6c38f43d274d706ee903563fefaa9
Robert van Gent <rvangent@google.com>
2018-11-06 14:46:06 -0800

    travis: always run Go tests for all modules, and wire check

---
+++ internal/testing/runchecks.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# Copyright 2018 The Go Cloud Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Runs only tests relevant to the current pull request.
+# At the moment, this only gates running the Wire test suite.
+# See https://github.com/google/go-cloud/issues/28 for solving the
+# general case.
+
+set -o pipefail
+
+if [[ $# -gt 0 ]]; then
+  echo "usage: allchecks.sh" 1>&2
+  exit 64
+fi
+
+module="github.com/google/go-cloud"
+
+# Run Go tests for each module.
+for path in "." "./internal/contributebot" "./samples/appengine"; do
+  echo Running tests in \"${path}\"
+  pushd ${path}
+  go test -race ./... || exit 1
+  popd
+done
+
+# Wire checks.
+go mod vendor || exit 1
+mapfile -t all_pkgs < <( go list "$module/..." ) || exit 1
+# TODO(light): Find out why the GO111MODULE=off override is necessary
+# and then remove it.
+echo "Running wire check on ${all_pkgs[@]}"
+GO111MODULE=off wire check "${all_pkgs[@]}" || exit 1
