00ebe7ca1f25cbbc58ddacbc612abcbbd6939c02
Issac Trotts <issactrotts@google.com>
2018-11-06 16:21:48 -0800

    paste contents of driver.go from design doc

--- pubsub/driver/driver.go
+++ pubsub/driver/driver.go
@@ -1,1 +1,42 @@
 package driver
+
+import "context"
+
+type AckID interface{}
+
+type Message struct {
+	// Body contains the content of the message.
+	Body []byte
+
+	// Attributes has key/value metadata for the message.
+	Attributes map[string]string
+
+	// AckID identifies the message on the server.
+	// It can be used to ack the message after it has been received.
+	AckID AckID
+}
+
+// Topic publishes messages.
+type Topic interface {
+	// SendBatch publishes all the messages in ms.
+	SendBatch(ctx context.Context, ms []*Message) error
+
+	// Close disconnects the Topic.
+	Close() error
+}
+
+// Subscription receives published messages.
+type Subscription interface {
+	// ReceiveBatch returns a batch of messages that have queued up for the
+	// subscription on the server.
+	ReceiveBatch(ctx context.Context) ([]*Message, error)
+
+	// SendAcks acknowledges the messages with the given ackIDs on the
+	// server so that they
+	// will not be received again for this subscription. This method
+	// returns only after all the ackIDs are sent.
+	SendAcks(ctx context.Context, ackIDs []interface{}) error
+
+	// Close disconnects the Subscription.
+	Close() error
+}
