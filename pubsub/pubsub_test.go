a8770bd463d0f2c67819c330c0f5c7e8f596a0f5
Issac Trotts <issactrotts@google.com>
2018-11-07 07:32:45 -0800

    add stubs of fake pubsub impl to test

--- pubsub/pubsub_test.go
+++ pubsub/pubsub_test.go
@@ -3,14 +3,54 @@ package pubsub_test
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/google/go-cloud/pubsub"
+	"github.com/google/go-cloud/pubsub/driver"
 )
+
+type driverTopic struct {
+	subs []*driverSub
+}
+
+func (t *driverTopic) SendBatch(ctx context.Context, ms []*driver.Message) error {
+	return nil
+}
+
+func (t *driverTopic) Close() error {
+	return nil
+}
+
+type driverSub struct {
+	q []*driver.Message
+}
+
+func (s *driverSub) ReceiveBatch(ctx context.Context) ([]*driver.Message, error) {
+	return nil, nil
+}
+
+func (s *driverSub) SendAcks(ctx context.Context, ackIDs []interface{}) error {
+	return nil
+}
+
+func (s *driverSub) Close() error {
+	return nil
+}
 
 func TestPubSubHappyPath(t *testing.T) {
 	ctx := context.Background()
-	topic := &pubsub.Topic{}
-	sub := &pubsub.Subscription{}
+	topicOpts := pubsub.TopicOptions{SendDelay: time.Millisecond, BatchSize: 10}
+	ds := &driverSub{}
+	dt := &driverTopic{
+		subs: []*driverSub{ds},
+	}
+	topic := pubsub.NewTopic(ctx, dt, topicOpts)
+	subOpts := pubsub.SubscriptionOptions{
+		AckDelay:     time.Millisecond,
+		AckBatchSize: 10,
+		AckDeadline:  time.Millisecond,
+	}
+	sub := pubsub.NewSubscription(ds, subOpts)
 	m := &pubsub.Message{Body: []byte("user signed up")}
 	if err := topic.Send(ctx, m); err != nil {
 		t.Fatal(err)
