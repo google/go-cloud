608b3a5f03304435f55d37c3ba980cc38a637e48
Jonathan Amsterdam <jba@google.com>
2018-11-16 19:06:36 -0500

    complete reimplementation

--- internal/batcher/batcher.go
+++ internal/batcher/batcher.go
@@ -14,7 +14,8 @@
 
 // Package batcher supports batching of items. Create a Batcher with a handler and
 // add items to it. Items are accumulated while handler calls are in progress; when
-// the handler returns, it will be called again with the accumulated items.
+// the handler returns, it will be called again with items accumulated since the last
+// call. Multiple concurrent calls to the handler are supported.
 package batcher
 
 import (
@@ -25,12 +26,13 @@ import (
 
 // A Batcher batches items.
 type Batcher struct {
+	maxHandlers   int
 	handler       func(interface{}) error
 	itemSliceZero reflect.Value // nil (zero value) for slice of items
-	sem           chan struct{} // semaphore for outstanding handlers
 
-	mu      sync.Mutex
-	pending []waiter // items waiting to be handled
+	mu        sync.Mutex
+	pending   []waiter // items waiting to be handled
+	nHandlers int      // number of currently running handler goroutines
 }
 
 type waiter struct {
@@ -43,71 +45,66 @@ type waiter struct {
 // itemExample is a value of the type that will be batched. For example, if you
 // want to create batches of *Entry, pass &Entry{} for itemExample.
 //
-// maxOutstandingHandlers is the maximum number of handlers that will run concurrently.
+// maxHandlers is the maximum number of handlers that will run concurrently.
 //
 // handler is a function that will be called on each bundle. If itemExample is
 // of type T, the argument to handler is of type []T.
-func New(itemExample interface{}, maxOutstandingHandlers int, handler func(interface{}) error) *Batcher {
-	b := &Batcher{
+func New(itemExample interface{}, maxHandlers int, handler func(interface{}) error) *Batcher {
+	return &Batcher{
+		maxHandlers:   maxHandlers,
 		handler:       handler,
-		sem:           make(chan struct{}, maxOutstandingHandlers),
 		itemSliceZero: reflect.Zero(reflect.SliceOf(reflect.TypeOf(itemExample))),
 	}
-	for i := 0; i < maxOutstandingHandlers; i++ {
-		b.sem <- struct{}{}
-	}
-	return b
 }
 
 // Add adds an item to the batcher. It blocks until the handler has
 // processed the item and reports the error that the handler returned.
 func (b *Batcher) Add(ctx context.Context, item interface{}) error {
-	// Create a channel to receive the error from the handler. Give it a buffer so
-	// that if a goroutine is in the middle of calling a handler when another
-	// finishes, the latter won't block sending to the channel.
+	// Create a channel to receive the error from the handler.
 	c := make(chan error, 1)
 	b.mu.Lock()
+	// Add the item to the pending list.
 	b.pending = append(b.pending, waiter{item, c})
+	if b.nHandlers < b.maxHandlers {
+		// If we can start a handler, do so with the item just added and any others that are pending.
+		go b.callHandler(b.pending)
+		b.pending = nil
+		b.nHandlers++
+	}
+	// If we can't start a handler, then one of the currently running handlers will
+	// take our item.
 	b.mu.Unlock()
-	// Keep processing batches until this item has been handled.
-	for {
-		// If there's a choice between doing work and getting the result for this
-		// item, prefer the result.
-		select {
-		case err := <-c:
-			return err
-		default:
-		}
-		// Wait until either we get a result or we can do some work.
-		select {
-		case err := <-c:
-			return err
-		case <-b.sem:
-			b.callHandler()
-			b.sem <- struct{}{}
-		}
+	// Wait until either our result is ready or the context is done.
+	select {
+	case err := <-c:
+		return err
+	case <-ctx.Done():
+		return ctx.Err()
 	}
 }
 
-// Invoke the handler on the slice of pending items.
-func (b *Batcher) callHandler() {
-	b.mu.Lock()
-	if len(b.pending) == 0 {
+func (b *Batcher) callHandler(batch []waiter) {
+	for batch != nil {
+		// Collect the items into a slice of the example type.
+		items := b.itemSliceZero
+		for _, m := range batch {
+			items = reflect.Append(items, reflect.ValueOf(m.item))
+		}
+		// Call the handler and report the result to all waiting
+		// callers of Add.
+		err := b.handler(items.Interface())
+		for _, m := range batch {
+			m.errc <- err
+		}
+		b.mu.Lock()
+		// If there is more work, keep running; otherwise exit. Take the new batch
+		// and decrement the handler count atomically, so that newly added items will
+		// always get to run.
+		batch = b.pending
+		b.pending = nil
+		if batch == nil {
+			b.nHandlers--
+		}
 		b.mu.Unlock()
-		return
-	}
-	batch := b.pending
-	b.pending = nil
-	b.mu.Unlock()
-	// Collect the items into a slice of the example type.
-	items := b.itemSliceZero
-	for _, m := range batch {
-		items = reflect.Append(items, reflect.ValueOf(m.item))
-	}
-	// Call the handler and report the result to all waiting
-	// callers of Add.
-	err := b.handler(items.Interface())
-	for _, m := range batch {
-		m.errc <- err
 	}
 }
