952123503b5b64da61397b0658997c219d5e4f7e
Issac Trotts <issactrotts@google.com>
2018-12-03 14:07:48 -0800

    fix compile errors on gcppubsub test

--- internal/pubsub/gcppubsub/as_test.go
+++ internal/pubsub/gcppubsub/as_test.go
@@ -5,12 +5,16 @@ import (
 	"testing"
 
 	raw "cloud.google.com/go/pubsub/apiv1"
+	"github.com/google/go-cloud/gcp"
 	"github.com/google/go-cloud/internal/pubsub/gcppubsub"
 )
 
+var proj gcp.ProjectID
+var topicName = "some-topic"
+var subName = "some-sub"
+
 func TestTopicAsCanFail(t *testing.T) {
-	proj := ""
-	topicName := "some-topic"
+	var client *raw.PublisherClient
 	top := gcppubsub.OpenTopic(context.Background(), client, proj, topicName, nil)
 	var c raw.PublisherClient
 	if top.As(&c) {
@@ -19,8 +23,7 @@ func TestTopicAsCanFail(t *testing.T) {
 }
 
 func TestTopicAsCanSucceed(t *testing.T) {
-	proj := ""
-	topicName := "some-topic"
+	var client *raw.PublisherClient
 	top := gcppubsub.OpenTopic(context.Background(), client, proj, topicName, nil)
 	var c *raw.PublisherClient
 	if !top.As(&c) {
@@ -29,21 +32,19 @@ func TestTopicAsCanSucceed(t *testing.T) {
 }
 
 func TestSubscriptionAsCanFail(t *testing.T) {
-	proj := ""
-	subName := "some-sub"
+	var client *raw.SubscriberClient
 	sub := gcppubsub.OpenSubscription(context.Background(), client, proj, subName, nil)
-	var c raw.SubscriptionClient
-	if top.As(&c) {
+	var c raw.SubscriberClient
+	if sub.As(&c) {
 		t.Errorf("cast succeeded for %T, want failure", &c)
 	}
 }
 
 func TestSubscriptionAsCanSucceed(t *testing.T) {
-	proj := ""
-	subName := "some-sub"
-	top := gcppubsub.OpenSubscription(context.Background(), client, proj, subName, nil)
-	var c *raw.SubscriptionClient
-	if !top.As(&c) {
+	var client *raw.SubscriberClient
+	sub := gcppubsub.OpenSubscription(context.Background(), client, proj, subName, nil)
+	var c *raw.SubscriberClient
+	if !sub.As(&c) {
 		t.Errorf("cast failed for %T", &c)
 	}
 }
