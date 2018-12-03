efc6b1bf0176405e4840aaadcef02fd0864e74bd
Issac Trotts <issactrotts@google.com>
2018-12-03 15:27:22 -0800

    combine some As tests

--- internal/pubsub/gcppubsub/as_test.go
+++ internal/pubsub/gcppubsub/as_test.go
@@ -13,44 +13,44 @@ var proj gcp.ProjectID
 var topicName = "some-topic"
 var subName = "some-sub"
 
-func TestTopicAsCanFail(t *testing.T) {
+func TestTopicAs(t *testing.T) {
 	var c *raw.PublisherClient
 	top := gcppubsub.OpenTopic(context.Background(), c, proj, topicName, nil)
-	var c2 raw.PublisherClient
-	if top.As(&c2) {
-		t.Errorf("cast succeeded for %T, want failure", &c2)
-	}
+	t.Run("can fail", func(t *testing.T) {
+		var c2 raw.PublisherClient
+		if top.As(&c2) {
+			t.Errorf("cast succeeded for %T, want failure", &c2)
+		}
+	})
+	t.Run("can succeed", func(t *testing.T) {
+		var c2 *raw.PublisherClient
+		if !top.As(&c2) {
+			t.Fatalf("cast failed for %T", &c2)
+		}
+		if c2 != c {
+			t.Errorf("got %p, want %p", c2, c)
+		}
+	})
 }
 
-func TestTopicAsCanSucceed(t *testing.T) {
-	var c *raw.PublisherClient
-	top := gcppubsub.OpenTopic(context.Background(), c, proj, topicName, nil)
-	var c2 *raw.PublisherClient
-	if !top.As(&c2) {
-		t.Fatalf("cast failed for %T", &c2)
-	}
-	if c2 != c {
-		t.Errorf("got %p, want %p", c2, c)
-	}
-}
-
-func TestSubscriptionAsCanFail(t *testing.T) {
+func TestSubscriptionAs(t *testing.T) {
 	var c *raw.SubscriberClient
 	sub := gcppubsub.OpenSubscription(context.Background(), c, proj, subName, nil)
-	var c2 raw.SubscriberClient
-	if sub.As(&c2) {
-		t.Errorf("cast succeeded for %T, want failure", &c2)
-	}
-}
-
-func TestSubscriptionAsCanSucceed(t *testing.T) {
-	var c *raw.SubscriberClient
-	sub := gcppubsub.OpenSubscription(context.Background(), c, proj, subName, nil)
-	var c2 *raw.SubscriberClient
-	if !sub.As(&c2) {
-		t.Fatalf("cast failed for %T", &c2)
-	}
-	if c2 != c {
-		t.Errorf("got %p, want %p", c2, c)
-	}
+	t.Run("can fail", func(t *testing.T) {
+		var c2 raw.SubscriberClient
+		if sub.As(&c2) {
+			t.Errorf("cast succeeded for %T, want failure", &c2)
+		}
+	})
+	t.Run("can succeed", func(t *testing.T) {
+		var c *raw.SubscriberClient
+		sub := gcppubsub.OpenSubscription(context.Background(), c, proj, subName, nil)
+		var c2 *raw.SubscriberClient
+		if !sub.As(&c2) {
+			t.Fatalf("cast failed for %T", &c2)
+		}
+		if c2 != c {
+			t.Errorf("got %p, want %p", c2, c)
+		}
+	})
 }
