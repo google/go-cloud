5885147c56c05ecf893124affec233a4ae5088a8
Issac Trotts <issactrotts@google.com>
2018-12-05 08:38:54 -0800

    pubsub: change Subscription.Close() to Shutdown(ctx) (#882)

--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -148,6 +148,9 @@ type Subscription struct {
 	// q is the local queue of messages downloaded from the server.
 	q   []*Message
 	err error
+
+	// cancel cancels all SendAcks calls.
+	cancel func()
 }
 
 // Receive receives and returns the next message from the Subscription's queue,
@@ -207,12 +210,21 @@ func (s *Subscription) getNextBatch(ctx context.Context) error {
 }
 
 // Close flushes pending ack sends and disconnects the Subscription.
-func (s *Subscription) Close() error {
+func (s *Subscription) Shutdown(ctx context.Context) error {
 	s.mu.Lock()
 	s.err = errors.New("pubsub: Subscription closed")
 	s.mu.Unlock()
-	s.ackBatcher.Shutdown()
-	return nil
+	c := make(chan struct{})
+	go func() {
+		defer close(c)
+		s.ackBatcher.Shutdown()
+	}()
+	select {
+	case <-ctx.Done():
+	case <-c:
+	}
+	s.cancel()
+	return ctx.Err()
 }
 
 // As converts i to provider-specific types. See provider documentation for
@@ -231,10 +243,10 @@ func (s *Subscription) As(i interface{}) bool {
 // periodically send them to the server.
 // It is for use by provider implementations.
 func NewSubscription(d driver.Subscription) *Subscription {
+	callCtx, cancel := context.WithCancel(context.Background())
 	handler := func(items interface{}) error {
 		ids := items.([]driver.AckID)
 		// TODO: Consider providing a way to stop this call. See #766.
-		callCtx := context.Background()
 		return retry.Call(callCtx, gax.Backoff{}, d.IsRetryable, func() error {
 			return d.SendAcks(callCtx, ids)
 		})
@@ -244,5 +256,6 @@ func NewSubscription(d driver.Subscription) *Subscription {
 	return &Subscription{
 		driver:     d,
 		ackBatcher: ab,
+		cancel:     cancel,
 	}
 }
