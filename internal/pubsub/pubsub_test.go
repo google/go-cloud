0c72b62e6f9f11120cc2a7916c91bbbcb830a2b9
Jonathan Amsterdam <jba@google.com>
2018-12-18 15:47:52 -0500

    add a test

--- internal/pubsub/pubsub_test.go
+++ internal/pubsub/pubsub_test.go
@@ -19,6 +19,7 @@ import (
 	"fmt"
 	"sync"
 	"testing"
+	"time"
 
 	"gocloud.dev/internal/pubsub"
 	"gocloud.dev/internal/pubsub/driver"
@@ -224,6 +225,52 @@ func TestCancelReceive(t *testing.T) {
 	// Without cancellation, this Receive would hang.
 	if _, err := s.Receive(ctx); err == nil {
 		t.Error("got nil, want cancellation error")
+	}
+}
+
+type blockingDriverSub struct {
+	driver.Subscription
+	inReceiveBatch chan int
+}
+
+func (b blockingDriverSub) ReceiveBatch(ctx context.Context, maxMessages int) ([]*driver.Message, error) {
+	b.inReceiveBatch <- 0
+	<-ctx.Done()
+	return nil, ctx.Err()
+}
+
+func TestCancelTwoReceives(t *testing.T) {
+	// We want to create the following situation:
+	// 1. Goroutine 1 calls Receive, obtains the lock (Subscription.mu),
+	//    and calls driver.ReceiveBatch, which hangs forever.
+	// 2. Goroutine 2 calls Receive.
+	// 3. The context passed to the Goroutine 2 call is canceled.
+	// We expect Goroutine 2's Receive to exit immediately. That won't
+	// happen if Receive holds the lock during the call to ReceiveBatch.
+	inReceiveBatch := make(chan int)
+	s := pubsub.NewSubscription(blockingDriverSub{inReceiveBatch: inReceiveBatch})
+	go func() {
+		s.Receive(context.Background())
+		panic("Receive should never return")
+	}()
+	<-inReceiveBatch
+	ctx, cancel := context.WithCancel(context.Background())
+	errc := make(chan error)
+	go func() {
+		_, err := s.Receive(ctx)
+		errc <- err
+	}()
+	// Give the Receive call time to block on the mutex before canceling.
+	time.AfterFunc(100*time.Millisecond, cancel)
+	select {
+	case err := <-errc:
+		if err != context.Canceled {
+			t.Errorf("got %v, want context.Canceled", err)
+		}
+		s.Shutdown(context.Background())
+	case <-time.After(time.Second):
+		t.Error("timed out waiting for canceled Receive to return")
+		// We can't call Shutdown in this case; s's mutex is held, so it would block forever.
 	}
 }
 
