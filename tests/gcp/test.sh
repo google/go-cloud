0e05523ccec2e4cd954457577a54801fe542a1a3
Robert van Gent <rvangent@google.com>
2019-01-25 11:02:03 -0800

    also .sh

--- tests/gcp/test.sh
+++ tests/gcp/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2018 The Go Cloud Authors
+# Copyright 2018 The Go Cloud Development Kit Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
