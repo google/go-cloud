9d7da3747f4e4efc9e43eddbed264c9cc656ff4b
Jonathan Amsterdam <jba@google.com>
2019-02-04 19:22:51 -0500

    .

--- pubsub/pubsub.go
+++ pubsub/pubsub.go
@@ -46,9 +46,11 @@
 //  - The internal driver methods SendBatch, SendAcks and ReceiveBatch.
 // All trace and metric names begin with the package import path.
 // The traces add the method name.
+// For example, "gocloud.dev/pubsub/Topic.Send".
 // The metrics are "completed_calls", a count of completed method calls by provider,
-// method and status (error code);  and "latency", a distribution of method latency
+// method and status (error code); and "latency", a distribution of method latency
 // by provider and method.
+// For example, "gocloud.dev/pubsub/latency".
 //
 // To enable trace collection in your application, see "Configure Exporter" at
 // https://opencensus.io/quickstart/go/tracing.
