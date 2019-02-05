549663ee504a5f7e5c6f1f484a3e808f1d989474
Jonathan Amsterdam <jba@users.noreply.github.com>
2019-02-04 19:29:23 -0500

    pubsub: improve OpenCensus doc (#1232)
    Give more info about traces and metrics.

--- pubsub/pubsub.go
+++ pubsub/pubsub.go
@@ -39,11 +39,18 @@
 // backend providers. See https://opencensus.io.
 //
 // This API collects OpenCensus traces and metrics for the following methods:
-// - Topic.Send
-// - Topic.Shutdown
-// - Subscription.Receive
-// - Subscription.Shutdown
-// - The internal driver methods SendBatch, SendAcks and ReceiveBatch.
+//  - Topic.Send
+//  - Topic.Shutdown
+//  - Subscription.Receive
+//  - Subscription.Shutdown
+//  - The internal driver methods SendBatch, SendAcks and ReceiveBatch.
+// All trace and metric names begin with the package import path.
+// The traces add the method name.
+// For example, "gocloud.dev/pubsub/Topic.Send".
+// The metrics are "completed_calls", a count of completed method calls by provider,
+// method and status (error code); and "latency", a distribution of method latency
+// by provider and method.
+// For example, "gocloud.dev/pubsub/latency".
 //
 // To enable trace collection in your application, see "Configure Exporter" at
 // https://opencensus.io/quickstart/go/tracing.
