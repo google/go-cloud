f5091b77a00d3f26efa6f2dc2e513c62e6667b11
Eli Bendersky <eliben@google.com>
2019-02-25 15:23:25 -0800

    Add "process" and "cleanup" labels

--- internal/admin/repository/main.tf
+++ internal/admin/repository/main.tf
@@ -131,6 +131,20 @@ resource "github_issue_label" "enhancement" {
   description = "New feature or request"
 }
 
+resource "github_issue_label" "process" {
+  repository  = "${github_repository.repo.name}"
+  name        = "process"
+  color       = "a2aaef"
+  description = "Improvement to the engineering process"
+}
+
+resource "github_issue_label" "cleanup" {
+  repository  = "${github_repository.repo.name}"
+  name        = "process"
+  color       = "a277ef"
+  description = "Maintenance or refactoring of the project"
+}
+
 resource "github_issue_label" "in_progress" {
   repository  = "${github_repository.repo.name}"
   name        = "in progress"
