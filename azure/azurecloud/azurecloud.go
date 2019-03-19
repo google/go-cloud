9e289bed08bae97ab68dbe7af985c1bcc81b2128
Robert van Gent <rvangent@google.com>
2019-03-19 16:19:23 -0700

    aws_azure: bring Wire provider sets for AWS and Azure up to date

--- azure/azurecloud/azurecloud.go
+++ azure/azurecloud/azurecloud.go
@@ -18,11 +18,24 @@ package azurecloud // import "gocloud.dev/azure/azurecloud"
 import (
 	"github.com/google/wire"
 	"gocloud.dev/blob/azureblob"
+	"gocloud.dev/pubsub/azuresb"
+	"gocloud.dev/secrets/azurekeyvault"
 )
 
 // Azure is a Wire provider set that includes the default wiring for all
 // Microsoft Azure services in this repository, but does not include
 // credentials. Individual services may require additional configuration.
 var Azure = wire.NewSet(
-	azureblob.NewPipeline,
+	azureblob.DefaultIdentity,
+	azureblob.SASTokenIdentity,
+	Services,
+)
+
+// Services is a Wire provider set that includes the default wiring for all
+// Azure services in this repository, but does not include credentials.
+// Individual services may require additional configuration.
+var Services = wire.NewSet(
+	azurekeyvault.Set,
+	azuresb.Set,
+	azureblob.Set,
 )
