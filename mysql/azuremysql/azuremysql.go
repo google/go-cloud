9d6db5594aa4a11a6e926fabff0200f252fbf90a
bpopovschi <zyqsempai@mail.ru>
2019-04-01 12:54:56 +0300

    Implement UrlOpener in Azure MySQL

--- mysql/azuremysql/azuremysql.go
+++ mysql/azuremysql/azuremysql.go
@@ -14,6 +14,17 @@
 
 // Package azuremysql provides connections to Azure Database for MySQL.
 // See https://docs.microsoft.com/en-us/azure/mysql.
+//
+// URLs
+//
+// For mysql.Open, azuremysql registers for the scheme "azuremysql".
+// The default URL opener will use credentials from the environment variables
+// AZURE_STORAGE_ACCOUNT, AZURE_STORAGE_KEY, and AZURE_STORAGE_SAS_TOKEN.
+// AZURE_STORAGE_ACCOUNT is required, along with one of the other two.
+// To customize the URL opener, or for more details on the URL format,
+// see URLOpener.
+//
+// See https://godoc.org/gocloud.dev#hdr-URLs for background information.
 package azuremysql // import "gocloud.dev/mysql/azuremysql"
 
 import (
@@ -22,11 +33,14 @@ import (
 	"database/sql"
 	"database/sql/driver"
 	"fmt"
+	"net/url"
+	"strings"
+	"sync"
 
 	"contrib.go.opencensus.io/integrations/ocsql"
 	"github.com/go-sql-driver/mysql"
-
 	"gocloud.dev/azure/azuredb"
+	gcmysql "gocloud.dev/mysql"
 )
 
 const (
@@ -46,6 +60,67 @@ type Params struct {
 	Database string
 	// TraceOpts contains options for OpenCensus.
 	TraceOpts []ocsql.TraceOption
+}
+
+func init() {
+	gcmysql.DefaultURLMux().RegisterMySQL(Scheme, &URLOpener{})
+}
+
+// URLOpener opens Azure MySQL URLs
+// like "azuremysql://user:password@mydemoserver.mysql.database.azure.com:3306/mydb".
+type URLOpener struct {
+	// CertSource specifies how the opener will obtain authentication information.
+	// CertSource must not be nil.
+	CertSource CertPoolProvider
+	// TraceOpts contains options for OpenCensus.
+	TraceOpts []ocsql.TraceOption
+}
+
+// Scheme is the URL scheme azuremysql registers its URLOpener under on
+// mysql.DefaultMux.
+const Scheme = "azuremysql"
+
+// lazyCredsOpener obtains Application Default Credentials on the first call
+// to OpenMySQLURL.
+type lazyCredsOpener struct {
+	init   sync.Once
+	opener *URLOpener
+	err    error
+}
+
+func (o *lazyCredsOpener) OpenMySQLURL(ctx context.Context, u *url.URL) (*sql.DB, error) {
+	o.init.Do(func() {
+		certSource, err := azuredb.NewAzureCertFetcherWithDefault()
+		if err != nil {
+			o.err = err
+			return
+		}
+		o.opener = &URLOpener{CertSource: certSource}
+	})
+	if o.err != nil {
+		return nil, fmt.Errorf("azuremysql open %v: %v", u, o.err)
+	}
+	return o.opener.OpenMySQLURL(ctx, u)
+}
+
+// OpenMySQLURL opens a new Azure database connection wrapped with OpenCensus instrumentation.
+func (uo *URLOpener) OpenMySQLURL(ctx context.Context, u *url.URL) (*sql.DB, error) {
+	if uo.CertSource == nil {
+		return nil, fmt.Errorf("azuremysql: URLOpener CertSource is nil")
+	}
+	password, _ := u.User.Password()
+	splitedHost := strings.Split(u.Host, ".")
+	params := &Params{
+		// Take only the server name from host
+		ServerName: splitedHost[0],
+		User:       u.User.Username(),
+		Password:   password,
+		// Remove "/" from DBPath
+		Database:  strings.TrimPrefix(u.Path, "/"),
+		TraceOpts: uo.TraceOpts,
+	}
+	db, _, err := Open(ctx, uo.CertSource, params)
+	return db, err
 }
 
 // GetFQDN constructs the FQDN for Azure Database for MySQL.
