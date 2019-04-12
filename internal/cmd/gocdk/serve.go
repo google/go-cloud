d90b159c03f785773ace3cbf80f4e987c60084ed
Ross Light <light@google.com>
2019-04-12 08:07:07 -0700

    internal/cmd/gocdk: add minimal serve command
    No live reloading, but waits for a healthy server.
    
    Fixes #1808

--- internal/cmd/gocdk/serve.go
+++ internal/cmd/gocdk/serve.go
@@ -16,10 +16,213 @@ package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
 
 	"golang.org/x/xerrors"
 )
 
 func serve(ctx context.Context, pctx *processContext, args []string) error {
-	return xerrors.New("not implemented")
+	f := newFlagSet(pctx, "serve")
+	if err := f.Parse(args); xerrors.Is(err, flag.ErrHelp) {
+		return nil
+	} else if err != nil {
+		return usagef("gocdk serve: %w", err)
+	}
+	if f.NArg() > 1 {
+		return usagef("gocdk serve [BIOME]")
+	}
+	biome := f.Arg(0)
+	if biome == "" {
+		biome = "dev"
+	}
+
+	// Check first that we're in a Go module.
+	moduleRoot, err := findModuleRoot(ctx, pctx.workdir)
+	if err != nil {
+		return xerrors.Errorf("gocdk serve: %w", err)
+	}
+	// Start main serve loop.
+	logger := log.New(pctx.stderr, "gocdk: ", log.Ldate|log.Ltime)
+	if err := serveLoop(ctx, pctx, logger, moduleRoot, biome); err != nil {
+		return xerrors.Errorf("gocdk serve: %w", err)
+	}
+	return nil
+}
+
+func serveLoop(ctx context.Context, pctx *processContext, logger *log.Logger, moduleRoot, biome string) error {
+	// Log biome that is being used.
+	logger.Printf("Preparing to serve %s...", biome)
+
+	// Create a temporary build directory.
+	buildDir, err := ioutil.TempDir("", "gocdk-build")
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := os.RemoveAll(buildDir); err != nil {
+			logger.Printf("Cleaning build: %v", err)
+		}
+	}()
+
+	// Apply Terraform configuration in biome.
+	if err := apply(ctx, pctx, []string{biome}); err != nil {
+		return err
+	}
+
+	// Build and run the server.
+	alloc := &serverAlloc{
+		exePath: filepath.Join(buildDir, "server"),
+		port:    9090,
+	}
+	logger.Println("Building server...")
+	if err := buildForServe(ctx, pctx, moduleRoot, alloc.exePath); err != nil {
+		return err
+	}
+	process, err := alloc.start(ctx, pctx, logger)
+	if err != nil {
+		return err
+	}
+	logger.Printf("Serving at %s", alloc.url(""))
+	// TODO(light): Loop on filesystem change.
+	<-ctx.Done()
+	logger.Println("Shutting down...")
+	if err := signalGracefulShutdown(process.Process); err != nil {
+		process.Process.Kill()
+	}
+	process.Wait()
+	return nil
+}
+
+// buildForServe runs Wire and `go build` at moduleRoot to create exePath.
+func buildForServe(ctx context.Context, pctx *processContext, moduleRoot string, exePath string) error {
+	moduleEnv := append(append([]string(nil), pctx.env...), "GO111MODULE=on")
+
+	if wireExe, err := exec.LookPath("wire"); err == nil {
+		// TODO(light): Only run Wire if needed, but that requires source analysis.
+		wireCmd := exec.CommandContext(ctx, wireExe, "./...")
+		wireCmd.Dir = moduleRoot
+		wireCmd.Env = moduleEnv
+		// TODO(light): Collect build logs into error.
+		wireCmd.Stdout = pctx.stderr
+		wireCmd.Stderr = pctx.stderr
+		if err := wireCmd.Run(); err != nil {
+			return xerrors.Errorf("build server: wire: %w", err)
+		}
+	}
+
+	buildCmd := exec.CommandContext(ctx, "go", "build", "-o", exePath)
+	buildCmd.Dir = moduleRoot
+	buildCmd.Env = moduleEnv
+	// TODO(light): Collect build logs into error.
+	buildCmd.Stdout = pctx.stderr
+	buildCmd.Stderr = pctx.stderr
+	if err := buildCmd.Run(); err != nil {
+		return xerrors.Errorf("build server: go build: %w", err)
+	}
+
+	return nil
+}
+
+// serverAlloc stores the built executable path and port for a single instance
+// of the user's application server.
+type serverAlloc struct {
+	exePath string
+	port    int
+}
+
+// url returns the URL to connect to the server with.
+func (alloc *serverAlloc) url(path string) *url.URL {
+	if path == "" {
+		path = "/"
+	}
+	return &url.URL{
+		Scheme: "http",
+		Host:   fmt.Sprintf("localhost:%d", alloc.port),
+		Path:   path,
+	}
+}
+
+// start starts the server process specified by the alloc and waits for
+// it to become healthy.
+func (alloc *serverAlloc) start(ctx context.Context, pctx *processContext, logger *log.Logger) (*exec.Cmd, error) {
+	// Run server.
+	logger.Print("Starting server...")
+	process := exec.Command(alloc.exePath, fmt.Sprintf("--address=localhost:%d", alloc.port))
+	process.Stdout = pctx.stdout
+	process.Stderr = pctx.stderr
+	if err := process.Start(); err != nil {
+		return nil, xerrors.Errorf("start server: %w", err)
+	}
+
+	// Server must report alive within 30 seconds.
+	aliveCtx, aliveCancel := context.WithTimeout(ctx, 30*time.Second)
+	err := waitForHealthy(aliveCtx, alloc.url("/healthz/liveness"))
+	aliveCancel()
+	if err != nil {
+		process.Process.Kill() // Send SIGKILL; no graceful shutdown.
+		process.Wait()
+		return nil, xerrors.Errorf("start server: %w", err)
+	}
+
+	// Wait for server to be ready.
+	logger.Printf("Waiting for server %s to report ready...", alloc.url("/"))
+	err = waitForHealthy(ctx, alloc.url("/healthz/readiness"))
+	if err != nil {
+		if err := signalGracefulShutdown(process.Process); err != nil {
+			process.Process.Kill()
+		}
+		process.Wait()
+		return nil, xerrors.Errorf("start server: %w", err)
+	}
+	return process, nil
+}
+
+// waitForHealthy polls a URL repeatedly until the server responds with a
+// non-server-error status, the context is canceled, or the context's deadline
+// is met. waitForHealthy returns an error in the latter two cases.
+func waitForHealthy(ctx context.Context, u *url.URL) error {
+	// Create health request.
+	// (Avoiding http.NewRequest to not reparse the URL.)
+	req := &http.Request{
+		Method: http.MethodGet,
+		URL:    u,
+	}
+	req = req.WithContext(ctx)
+
+	// Poll for healthy.
+	tick := time.NewTicker(100 * time.Millisecond)
+	defer tick.Stop()
+	for {
+		// Check response. Allow 200-level (success) or 400-level (client error)
+		// status codes. The latter is permitted for the case where the application
+		// doesn't serve explicit health checks.
+		resp, err := http.DefaultClient.Do(req)
+		if err == nil && (statusCodeInRange(resp.StatusCode, 200) || statusCodeInRange(resp.StatusCode, 400)) {
+			return nil
+		}
+		// Wait for the next tick.
+		select {
+		case <-tick.C:
+		case <-ctx.Done():
+			return xerrors.Errorf("wait for healthy: %w", ctx.Err())
+		}
+	}
+}
+
+// statusCodeInRange reports whether the given HTTP status code is in the range
+// [start, start+100).
+func statusCodeInRange(statusCode, start int) bool {
+	if start < 100 || start%100 != 0 {
+		panic("statusCodeInRange start must be a multiple of 100")
+	}
+	return start <= statusCode && statusCode < start+100
 }
