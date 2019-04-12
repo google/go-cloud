e769b6de29819847c8b694d6c1becb3bd9213b00
Ross Light <light@google.com>
2019-04-12 09:55:42 -0700

    Update samples/server/main.go
    Co-Authored-By: eliben <eliben@users.noreply.github.com>

--- samples/server/main.go
+++ samples/server/main.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Command server runs a simple HTTP server with integrated StackDriver tracing
+// Command server runs a simple HTTP server with integrated Stackdriver tracing
 // and health checks.
 package main
 
