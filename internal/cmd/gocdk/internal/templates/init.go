e364c68b51be6420fe064c97f3aa6cc42de47827
Ross Light <light@google.com>
2019-04-23 14:01:22 -0700

    Add a basic server

--- internal/cmd/gocdk/internal/templates/init.go
+++ internal/cmd/gocdk/internal/templates/init.go
@@ -29,7 +29,27 @@ var InitTemplates = map[string]string{
 
 	"main.go": `package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	http.HandleFunc("/", greet)
+	if err := http.ListenAndServe(":" + port, nil); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
 `,
 
 	"biomes/dev/biome.json": `{
