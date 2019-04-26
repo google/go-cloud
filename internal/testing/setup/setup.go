90eb88b688afb8fb0fad9e16952e543a8fadeb3e
Eli Bendersky <eliben@google.com>
2019-04-26 10:11:32 -0700

    Rerun record for newly added test + fix typo

--- internal/testing/setup/setup.go
+++ internal/testing/setup/setup.go
@@ -189,14 +189,14 @@ func NewGCPClient(ctx context.Context, t *testing.T) (client *gcp.HTTPClient, rt
 	if err != nil {
 		t.Fatal(err)
 	}
-	client, err := replay.Client(ctx)
+	c, err := replay.Client(ctx)
 	if err != nil {
 		t.Fatal(err)
 	}
 	cleanup := func() {
 		_ = replay.Close()
 	}
-	return &gcp.HTTPClient{Client: *client}, client.Transport, cleanup
+	return &gcp.HTTPClient{Client: *c}, c.Transport, cleanup
 }
 
 // NewGCPgRPCConn creates a new connection for testing against GCP via gRPC.
