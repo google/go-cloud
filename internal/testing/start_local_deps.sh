1cb30eec482df6aaf7b88ef5659e1ddb37a197d8
Robert van Gent <rvangent@google.com>
2019-04-26 10:15:32 -0700

    update comment

--- internal/testing/start_local_deps.sh
+++ internal/testing/start_local_deps.sh
@@ -16,7 +16,7 @@
 # Starts all local instances needed for Go CDK tests.
 # You must have Docker installed.
 # Run this script from the top level of the tree, e.g.:
-#   ./internal/testing/all_local.sh
+#   ./internal/testing/start_local_deps.sh
 
 # https://coderwall.com/p/fkfaqq/safer-bash-scripts-with-set-euxo-pipefail
 set -euo pipefail
