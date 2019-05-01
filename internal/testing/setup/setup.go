58d3b2e9b2962157e5a2cc28444e3bf2c841b0f8
Eli Bendersky <eliben@google.com>
2019-05-01 13:53:50 -0700

    PR comment

--- internal/testing/setup/setup.go
+++ internal/testing/setup/setup.go
@@ -95,14 +95,12 @@ func awsSession(region string, client *http.Client) (*session.Session, error) {
 	})
 }
 
-type RecorderModifier func(r *httpreplay.Recorder)
-
 // NewRecordReplayClient creates a new http.Client for tests. This client's
 // activity is being either recorded to files (when *Record is set) or replayed
 // from files. rf is a modifier function that will be invoked with the address
 // of the httpreplay.Recorder object used to obtain the client; this function
 // can mutate the recorder to add provider-specific header filters, for example.
-func NewRecordReplayClient(ctx context.Context, t *testing.T, rf RecorderModifier, opts ...option.ClientOption) (c *http.Client, cleanup func()) {
+func NewRecordReplayClient(ctx context.Context, t *testing.T, rf func(r *httpreplay.Recorder), opts ...option.ClientOption) (c *http.Client, cleanup func()) {
 	httpreplay.DebugHeaders()
 	path := filepath.Join("testdata", t.Name()+".replay")
 	if *Record {
