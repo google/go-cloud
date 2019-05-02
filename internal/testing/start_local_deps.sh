e1154b8a9f76462f291d98368c1ced4f5540550e
Robert van Gent <rvangent@google.com>
2019-05-02 15:17:46 -0700

    fix path to etcd start script

--- internal/testing/start_local_deps.sh
+++ internal/testing/start_local_deps.sh
@@ -23,6 +23,6 @@ set -euo pipefail
 
 ./pubsub/kafkapubsub/localkafka.sh
 ./pubsub/rabbitpubsub/localrabbit.sh
-./runtimevar/etcd/localetcd.sh
+./runtimevar/etcdvar/localetcd.sh
 ./internal/docstore/mongodocstore/localmongo.sh
 
