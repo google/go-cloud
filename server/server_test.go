c9963aff55834e130bb6833171f423caf208d4c1
Shouichi Kamiya <shouichi.kamiya@gmail.com>
2019-05-04 04:21:07 +0900

    server: fix to start span before logging (#1977)
    Fixes #1976

--- server/server_test.go
+++ server/server_test.go
@@ -18,7 +18,10 @@ import (
 	"context"
 	"errors"
 	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"gocloud.dev/requestlog"
 )
 
 func TestListenAndServe(t *testing.T) {
@@ -31,17 +34,64 @@ func TestListenAndServe(t *testing.T) {
 	if !td.listenAndServeCalled {
 		t.Error("ListenAndServe was not called from the supplied driver")
 	}
+	if td.handler == nil {
+		t.Error("testDriver must set handler, got nil")
+	}
+}
+
+func TestMiddleware(t *testing.T) {
+	onLogCalled := false
+
+	tl := &testLogger{
+		onLog: func(ent *requestlog.Entry) {
+			onLogCalled = true
+			if ent.TraceID.String() == "" {
+				t.Error("TraceID is empty")
+			}
+			if ent.SpanID.String() == "" {
+				t.Error("SpanID is empty")
+			}
+		},
+	}
+
+	td := new(testDriver)
+	s := New(http.NotFoundHandler(), &Options{Driver: td, RequestLogger: tl})
+	err := s.ListenAndServe(":8080")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	td.handler.ServeHTTP(rr, req)
+	if !onLogCalled {
+		t.Fatal("logging middleware was not called")
+	}
 }
 
 type testDriver struct {
 	listenAndServeCalled bool
+	handler              http.Handler
 }
 
 func (td *testDriver) ListenAndServe(addr string, h http.Handler) error {
 	td.listenAndServeCalled = true
+	td.handler = h
 	return nil
 }
 
 func (td *testDriver) Shutdown(ctx context.Context) error {
 	return errors.New("this is a method for satisfying the interface")
+}
+
+type testLogger struct {
+	onLog func(ent *requestlog.Entry)
+}
+
+func (tl *testLogger) Log(ent *requestlog.Entry) {
+	tl.onLog(ent)
 }
