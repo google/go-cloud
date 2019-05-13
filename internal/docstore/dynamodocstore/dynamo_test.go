135e1fbc6ec2e29c37f11f853d45e5da09b38b56
Jonathan Amsterdam <jba@google.com>
2019-05-13 16:31:52 -0400

    See #2051.

--- internal/docstore/dynamodocstore/dynamo_test.go
+++ internal/docstore/dynamodocstore/dynamo_test.go
@@ -176,6 +176,7 @@ func TestProcessURL(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
+
 		_, _, _, _, _, err = o.processURL(u)
 		if (err != nil) != test.WantErr {
 			t.Errorf("%s: got error %v, want error %v", test.URL, err, test.WantErr)
