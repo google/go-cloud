9f5e20287f2a1b725d2db63b63a06e49c2d2f2f2
Jonathan Amsterdam <jba@google.com>
2019-05-20 11:39:51 -0400

    docstore: support struct tags
    Add support for tags with keys "docstore".
    
    Support "-" to mean omit field, and "omitempty" to mean omit the field
    if the value is empty (using the same code as in encoding/json).

--- internal/docstore/dynamodocstore/query.go
+++ internal/docstore/dynamodocstore/query.go
@@ -77,7 +77,7 @@ func (c *collection) planQuery(q *driver.Query) (*queryRunner, error) {
 		}
 		if !hasRevisionField {
 			pb = pb.AddNames(expression.Name(docstore.RevisionField))
-			q.FieldPaths = append(q.FieldPaths, []string{docstore.RevisionField})
+			q.FieldPaths = append(q.FieldPaths, []string{docstore.RevisionFieldo})
 		}
 		cb = cb.WithProjection(pb)
 		cbUsed = true
