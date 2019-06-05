8b34cfabaff2dcc0d025f17c178fe8a61c670e24
Eli Bendersky <eliben@gmail.com>
2019-06-05 09:53:01 -0700

    Refactor releasehelper for easier testing and add a test.
    The refactoring is to test both runOnGomod and the handlers in a black-box
    way. The test creates a temporary directory simulating a small subset of
    our repository.

--- internal/testing/releasehelper.go
+++ internal/testing/releasehelper.go
@@ -156,25 +156,33 @@ func runOnGomod(path string, reqHandler reqHandlerFunc) {
 	}
 }
 
+func gomodAddReplace(path string) {
+	runOnGomod(path, func(gomodPath, mod, modPath string) {
+		cmdCheck(fmt.Sprintf("go mod edit -replace=%s=%s %s", mod, modPath, gomodPath))
+	})
+}
+
+func gomodDropReplace(path string) {
+	runOnGomod(path, func(gomodPath, mod, modPath string) {
+		cmdCheck(fmt.Sprintf("go mod edit -dropreplace=%s %s", mod, gomodPath))
+	})
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		printHelp()
 		os.Exit(0)
 	}
 
-	var handlerFunc reqHandlerFunc
+	var gomodHandler func(path string)
 	switch os.Args[1] {
 	case "help":
 		printHelp()
 		os.Exit(0)
 	case "addreplace":
-		handlerFunc = func(gomodPath, mod, modPath string) {
-			cmdCheck(fmt.Sprintf("go mod edit -replace=%s=%s %s", mod, modPath, gomodPath))
-		}
+		gomodHandler = gomodAddReplace
 	case "dropreplace":
-		handlerFunc = func(gomodPath, mod, modPath string) {
-			cmdCheck(fmt.Sprintf("go mod edit -dropreplace=%s %s", mod, gomodPath))
-		}
+		gomodHandler = gomodDropReplace
 	default:
 		printHelp()
 		os.Exit(1)
@@ -189,7 +197,7 @@ func main() {
 	input.Split(bufio.ScanLines)
 	for input.Scan() {
 		if len(input.Text()) > 0 && !strings.HasPrefix(input.Text(), "#") {
-			runOnGomod(input.Text(), handlerFunc)
+			gomodHandler(input.Text())
 		}
 	}
 
