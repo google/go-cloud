a8e134cace63986e0945d8cb9980b23d429d84c8
Robert van Gent <rvangent@google.com>
2019-06-07 15:32:34 -0700

    use bucket.region
    Co-Authored-By: Ross Light <light@google.com>

--- internal/cmd/gocdk/static/_assets/provision/blob/s3blob/s3blob.tf
+++ internal/cmd/gocdk/static/_assets/provision/blob/s3blob/s3blob.tf
@@ -1,7 +1,7 @@
 # TODO(rvangent): Add comments explaining.
 
 locals {
-  s3blob_bucket_url = "s3://${aws_s3_bucket.bucket.id}?region=${var.aws_region}"
+  s3blob_bucket_url = "s3://${aws_s3_bucket.bucket.id}?region=${aws_s3_bucket.bucket.region}"
 }
 
 resource "aws_s3_bucket" "bucket" {
