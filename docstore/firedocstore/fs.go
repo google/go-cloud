362ec992b7274172c29ef96eb0bd8a3002d511dd
Jonathan Amsterdam <jba@users.noreply.github.com>
2019-06-14 17:07:52 -0400

    docstore/firedocstore: explain uses for name function (#2343)
    Fixes #2297.

--- docstore/firedocstore/fs.go
+++ docstore/firedocstore/fs.go
@@ -106,9 +106,9 @@ func CollectionResourceID(projectID, collPath string) string {
 // collection, like "States/Wisconsin/Cities".
 // See https://cloud.google.com/firestore/docs/reference/rest/ for more detail.
 //
-// firedocstore requires that a single string field, nameField, be designated the
-// primary key. Its values must be unique over all documents in the collection, and
-// the primary key must be provided to retrieve a document.
+// firedocstore requires that a single field, nameField, be designated the primary
+// key. Its values must be strings, and must be unique over all documents in the
+// collection. The primary key must be provided to retrieve a document.
 func OpenCollection(client *vkit.Client, collResourceID, nameField string, opts *Options) (*docstore.Collection, error) {
 	c, err := newCollection(client, collResourceID, nameField, nil, opts)
 	if err != nil {
@@ -127,6 +127,10 @@ func OpenCollection(client *vkit.Client, collResourceID, nameField string, opts 
 // be used for the document's primary key. It should return the empty string if the
 // document is missing the information to construct a name. This will cause all
 // actions, even Create, to fail.
+//
+// Providing a function to construct the primary key is useful in two situations: if
+// your desired primary key field is not a string, or if there is more than one field
+// you want to use as a primary key.
 func OpenCollectionWithNameFunc(client *vkit.Client, collResourceID string, nameFunc func(docstore.Document) string, opts *Options) (*docstore.Collection, error) {
 	c, err := newCollection(client, collResourceID, "", nameFunc, opts)
 	if err != nil {
