8c62b53ed0cc1a0ad750f116f0ef806f35d04428
Robert van Gent <rvangent@google.com>
2019-06-14 13:12:52 -0700

    keep skip

--- internal/cmd/gocdk/apply_test.go
+++ internal/cmd/gocdk/apply_test.go
@@ -18,6 +18,7 @@ import (
 	"context"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strconv"
 	"testing"
@@ -31,6 +32,10 @@ func TestApply(t *testing.T) {
 	// terraform not initialized *
 	// terraform never previously applied
 	// terraform apply repeat
+
+	if _, err := exec.LookPath("terraform"); err != nil {
+		t.Skip("terraform not found:", err)
+	}
 
 	t.Run("TerraformNotInitialized", func(t *testing.T) {
 		ctx := context.Background()
