1012b9d3127e9fd964c92cb870f24860bda4c72a
Ross Light <light@google.com>
2019-06-26 13:56:12 -0700

    internal/website/howto: add SQL how-to guides (#2392)
    Added examples to packages that didn't have them. Fixed an annoyance
    in gatherexamples/run.sh where the working directory mattered.
    
    Fixes #1152
    Fixes #2364

--- mysql/azuremysql/example_test.go
+++ mysql/azuremysql/example_test.go
@@ -1,4 +1,4 @@
-// Copyright 2018 The Go Cloud Development Kit Authors
+// Copyright 2019 The Go Cloud Development Kit Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,32 +12,32 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package awsmysql_test
+package azuremysql_test
 
 import (
 	"context"
 	"log"
 
 	"gocloud.dev/mysql"
-	_ "gocloud.dev/mysql/awsmysql"
+	_ "gocloud.dev/mysql/azuremysql"
 )
 
 func Example() {
-	// This example is used in https://gocloud.dev/howto/sql/#aws
+	// This example is used in https://gocloud.dev/howto/sql/#azure
 
-	// import _ "gocloud.dev/mysql/awsmysql"
+	// import _ "gocloud.dev/mysql/azuremysql"
 
 	// Variables set up elsewhere:
 	ctx := context.Background()
 
-	// Replace these with your actual settings.
+	// Replace this with your actual settings.
 	db, err := mysql.Open(ctx,
-		"awsmysql://myrole:swordfish@example01.xyzzy.us-west-1.rds.amazonaws.com/testdb")
+		"azuremysql://user:password@example00.mysql.database.azure.com/testdb")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
 	// Use database in your program.
-	db.ExecContext(ctx, "CREATE TABLE foo (bar INT);")
+	db.Exec("CREATE TABLE foo (bar INT);")
 }
