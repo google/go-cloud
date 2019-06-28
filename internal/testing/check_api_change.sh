02b7ef641880a69f94a1631075d3fd12d67ef60c
Jonathan Amsterdam <jba@google.com>
2019-06-28 09:33:42 -0400

    BREAKING_CHANGE_OK

--- internal/testing/check_api_change.sh
+++ internal/testing/check_api_change.sh
@@ -100,11 +100,6 @@ echo "Found breaking API change(s) in: ${incompatible_change_pkgs[*]}."
 
 # Found incompatible changes; see if they were declared as OK via a commit.
 cd "$ORIG_DIR"
-
-echo "--- git cherry -v master ---"
-git cherry -v master
-echo "----------------------------"
-
 if git cherry -v master | grep -q "BREAKING_CHANGE_OK"; then
   echo "Allowing them due to a commit message with BREAKING_CHANGE_OK.";
   exit 0;
