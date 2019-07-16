be325655f0239f2338aa491e53a73f1fe479bd03
Robert van Gent <rvangent@google.com>
2019-07-16 10:35:48 -0700

    internal/gocdk: add comments in Terraform files (#2563)

--- internal/cmd/gocdk/internal/static/_assets/resource/docstore/azurecosmos.tf
+++ internal/cmd/gocdk/internal/static/_assets/resource/docstore/azurecosmos.tf
@@ -1,10 +1,16 @@
-# TODO(rvangent): Add comments explaining.
+# This file creates an Azure CosmoDB database for use with the
+# "docstore/mongodocstore" driver.
 
 locals {
-  azurecosmos_url       = "mongo://demo-db/demo-collection?id_field=Key"
+  # The Go CDK URL for the database: https://gocloud.dev/howto/docstore/#cosmos.
+  azurecosmos_url = "mongo://demo-db/demo-collection?id_field=Key"
+  # The MONGO_SERVER_URL to be used to connect to the CosmosDB using a Mongo client.
   azurecosmos_mongo_url = azurerm_cosmosdb_account.account.connection_strings[0]
 }
 
+# A CosmosDB account.
+# Databases and collections are created automatically within the account, so
+# there's no need to provision them explicitly.
 resource "azurerm_cosmosdb_account" "account" {
   name                = local.gocdk_random_name
   location            = azurerm_resource_group.resource_group.location
