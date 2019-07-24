ac7983cdad84e6ec5d231627a52551fa0b595d53
Ross Light <ross@zombiezen.com>
2019-07-24 14:39:45 -0700

    internal/cmd/gocdk: pass around biomeApplyOptions by value
    Eliminates possibility of nil pointer panic when accessing apply
    options. I also slightly tweaked the fields such that the zero value is
    the same as the default flags, so that functions that call with the zero
    options value will provide the same inputs.
    
    Fixes #2607

--- internal/cmd/gocdk/deploy.go
+++ internal/cmd/gocdk/deploy.go
@@ -72,7 +72,7 @@ func deploy(ctx context.Context, pctx *processContext, biome, dockerImage string
 
 	// Run "biome apply".
 	if apply {
-		if err := biomeApply(ctx, pctx, biome, nil); err != nil {
+		if err := biomeApply(ctx, pctx, biome, biomeApplyOptions{}); err != nil {
 			return err
 		}
 	}
