4d9a5d15dd1b7b6dabf732e71561ff45f279a0b6
Jonathan Amsterdam <jba@google.com>
2019-08-03 06:01:35 -0400

    BREAKING_CHANGE_OK

--- docstore/query.go
+++ docstore/query.go
@@ -267,7 +267,7 @@ func (q *Query) runActionsWithRetry(ctx context.Context, addAction func(*ActionL
 
 // Update updates all the documents specified by the query.
 // It is an error if the query has a limit.
-// TODO: unexport.
+// TODO(shantuo): unexport.
 func (q *Query) Update(ctx context.Context, mods Mods) error {
 	if err := q.validateWrite("update"); err != nil {
 		return err
