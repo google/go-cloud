297c0c7b4d0a041b5d4f3dce16b237313878ad98
Robert van Gent <rvangent@google.com>
2019-08-05 14:04:10 -0700

    extend driver interface comment

--- blob/driver/driver.go
+++ blob/driver/driver.go
@@ -306,7 +306,8 @@ type Bucket interface {
 type SignedURLOptions struct {
 	// Expiry sets how long the returned URL is valid for. It is guaranteed to be > 0.
 	Expiry time.Duration
-	// Method is the HTTP method that can be used on the URL; one of "GET", "PUT", or "DELETE".
+	// Method is the HTTP method that can be used on the URL; one of "GET", "PUT",
+	// or "DELETE". Drivers must implement all 3.
 	Method string
 }
 
