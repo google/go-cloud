61f2150e3d63fcb3adf1b63fc3b7b3ae142dbea9
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2019-12-27 03:32:52 +0700

    blob/gcsblob: use STORAGE_EMULATOR_HOST if present (#2719)

--- blob/gcsblob/gcsblob.go
+++ blob/gcsblob/gcsblob.go
@@ -55,6 +55,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"os"
 	"sort"
 	"strings"
 	"sync"
@@ -62,15 +63,17 @@ import (
 
 	"cloud.google.com/go/storage"
 	"github.com/google/wire"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/googleapi"
+	"google.golang.org/api/iterator"
+	"google.golang.org/api/option"
+
 	"gocloud.dev/blob"
 	"gocloud.dev/blob/driver"
 	"gocloud.dev/gcerrors"
 	"gocloud.dev/gcp"
 	"gocloud.dev/internal/escape"
 	"gocloud.dev/internal/useragent"
-	"google.golang.org/api/googleapi"
-	"google.golang.org/api/iterator"
-	"google.golang.org/api/option"
 )
 
 const defaultPageSize = 1000
@@ -94,11 +97,18 @@ type lazyCredsOpener struct {
 
 func (o *lazyCredsOpener) OpenBucketURL(ctx context.Context, u *url.URL) (*blob.Bucket, error) {
 	o.init.Do(func() {
-		creds, err := gcp.DefaultCredentials(ctx)
-		if err != nil {
-			o.err = err
-			return
+		var creds *google.Credentials
+		if os.Getenv("STORAGE_EMULATOR_HOST") != "" {
+			creds, _ = google.CredentialsFromJSON(ctx, []byte(`{"type": "service_account", "project_id": "my-project-id"}`))
+		} else {
+			var err error
+			creds, err = gcp.DefaultCredentials(ctx)
+			if err != nil {
+				o.err = err
+				return
+			}
 		}
+
 		client, err := gcp.NewHTTPClient(gcp.DefaultTransport(), creds.TokenSource)
 		if err != nil {
 			o.err = err
@@ -189,8 +199,18 @@ func openBucket(ctx context.Context, client *gcp.HTTPClient, bucketName string, 
 	if bucketName == "" {
 		return nil, errors.New("gcsblob.OpenBucket: bucketName is required")
 	}
+
+	clientOpts := []option.ClientOption{option.WithHTTPClient(useragent.HTTPClient(&client.Client, "blob"))}
+	if host := os.Getenv("STORAGE_EMULATOR_HOST"); host != "" {
+		clientOpts = []option.ClientOption{
+			option.WithoutAuthentication(),
+			option.WithEndpoint("http://" + host + "/storage/v1/"),
+			option.WithHTTPClient(http.DefaultClient),
+		}
+	}
+
 	// We wrap the provided http.Client to add a Go CDK User-Agent.
-	c, err := storage.NewClient(ctx, option.WithHTTPClient(useragent.HTTPClient(&client.Client, "blob")))
+	c, err := storage.NewClient(ctx, clientOpts...)
 	if err != nil {
 		return nil, err
 	}
