9ba6cbe3f359621f77f8542d8e75eb83d87104ec
Robert van Gent <rvangent@google.com>
2022-10-13 08:22:47 -0700

    Revert "gcp/cloudsql: enable IAM login (#3122)"
    This reverts commit 452381bb74ab35ec65f6e50261f2e61f35b81149.

--- gcp/cloudsql/cloudsql.go
+++ gcp/cloudsql/cloudsql.go
@@ -32,5 +32,5 @@ var CertSourceSet = wire.NewSet(
 // NewCertSource creates a local certificate source that uses the given
 // HTTP client. The client is assumed to make authenticated requests.
 func NewCertSource(c *gcp.HTTPClient) *certs.RemoteCertSource {
-	return certs.NewCertSourceOpts(&c.Client, certs.RemoteOpts{EnableIAMLogin: true})
+	return certs.NewCertSourceOpts(&c.Client, certs.RemoteOpts{})
 }
