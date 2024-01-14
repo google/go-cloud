060912ac03f2e9a94486369d20ae1046563d731a
Bart Venter <72999113+bartventer@users.noreply.github.com>
2024-01-14 08:50:16 +0200

    fix: update Filter.Op comments for new in/not-in operators

--- docstore/driver/driver.go
+++ docstore/driver/driver.go
@@ -214,7 +214,7 @@ type Query struct {
 // TODO(#1762): support comparison of other types.
 type Filter struct {
 	FieldPath []string    // the field path to filter
-	Op        string      // the operation, supports =, >, >=, <, <=
+	Op        string      // the operation, supports `=`, `>`, `>=`, `<`, `<=`, `in`, `not-in`
 	Value     interface{} // the value to compare using the operation
 }
 
