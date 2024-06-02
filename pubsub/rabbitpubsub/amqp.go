cd7eb9473383c52c15713783802127fbf7a32633
Tiago Peczenyj <tpeczenyj@weborama.com>
2024-06-02 23:06:55 +0200

    use constant instead hard coded string

--- pubsub/rabbitpubsub/amqp.go
+++ pubsub/rabbitpubsub/amqp.go
@@ -136,10 +136,10 @@ func (ch *channel) NotifyClose(c chan *amqp.Error) chan *amqp.Error {
 
 func (ch *channel) ExchangeDeclare(name string) error {
 	return ch.ch.ExchangeDeclare(name,
-		"fanout", // kind
-		false,    // durable
-		false,    // delete when unused
-		false,    // internal
+		amqp.ExchangeFanout, // kind
+		false,               // durable
+		false,               // delete when unused
+		false,               // internal
 		wait,
 		nil) // args
 }
