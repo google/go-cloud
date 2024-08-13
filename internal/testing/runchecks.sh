475f86bf6cd7fb04ee67e6f4ddbe1ee305b5bc72
Robert van Gent <rvangent@google.com>
2024-08-13 20:23:23 +0000

    all: update to go version 1.23

--- internal/testing/runchecks.sh
+++ internal/testing/runchecks.sh
@@ -44,7 +44,7 @@ rootdir="$(pwd)"
 # new Go version. Some checks below we only run
 # for the latest Go version.
 latest_go_version=0
-if [[ $(go version) == *go1\.22* ]]; then
+if [[ $(go version) == *go1\.23* ]]; then
   latest_go_version=1
 fi
 
