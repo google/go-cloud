4e5f5dc8e47332df95bb5d7abd3343ca4e9b844e
sandeepvinayak <sandeep.pal@outlook.com>
2025-03-10 11:38:51 -0700

    mongodb to single replica replicaset

--- docstore/mongodocstore/localmongo.sh
+++ docstore/mongodocstore/localmongo.sh
@@ -19,10 +19,11 @@
 # https://coderwall.com/p/fkfaqq/safer-bash-scripts-with-set-euxo-pipefail
 set -euo pipefail
 
-echo "Starting MongoDB v4 listening on 27017, 27018, 27019..."
-docker rm -f mongo41 mongo42 mongo43 mongosetup &> /dev/null || :
-docker compose up --wait &> /dev/null
-echo "...done. Run \"docker rm -f mongo41 mongo42 mongo43 mongosetup\" to clean up the container."
+echo "Starting MongoDB v4 listening on 27017..."
+docker rm -f mongo4 mongosetup &> /dev/null || :
+docker compose -f ./docstore/mongodocstore/docker-compose.yml up --wait &> /dev/null
+sleep 3
+echo "...done. Run \"docker rm -f mongo4 mongosetup\" to clean up the container."
 echo
 
 echo "Starting MongoDB v3 listening on 27020..."
