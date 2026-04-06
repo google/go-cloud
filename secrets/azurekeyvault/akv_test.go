783eb88d604700f67e4e119b3f3c02eb212ab595
Robert van Gent <rvangent@google.com>
2026-04-06 16:33:29 +0000

    ckpt

--- secrets/azurekeyvault/akv_test.go
+++ secrets/azurekeyvault/akv_test.go
@@ -24,7 +24,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
-	"github.com/Azure/azure-sdk-for-go/sdk/keyvault/azkeys"
+	"github.com/Azure/azure-sdk-for-go/sdk/security/keyvault/azkeys"
 	"gocloud.dev/internal/testing/setup"
 	"gocloud.dev/internal/useragent"
 	"gocloud.dev/secrets"
